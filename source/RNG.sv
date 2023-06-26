dec6791fce7a5335743ead79fcc7710db5ed7c3c
Andre Ponsot <ponsot@purdue.edu>
2023-06-26 10:31:41 -0400

    Balla

--- source/RNG.sv
+++ source/RNG.sv
@@ -1,6 +1,7 @@
 module rng( 
     input logic clk, nrst,
-    output reg [15:0] out, [5:0] note  
+    output logic [15:0] out,
+    output logic [5:0] note
 );
     logic feedback;
     assign feedback= ~(out[10]^(out[12]^(out[13]^out[15])));
@@ -12,14 +13,21 @@ module rng(
             out <= 16'b1010010001010101;
         else
             out <= {out[14:0],feedback}; 
-    end  
-    always_ff @ (posedge clk, negedge nrst) begin 
+    end
+
+    logic [5:0] next_note;
+
+    always_comb begin
+        next_note = {note[4:0],feedback_2};
         if (out[15] == 1)
-            note <= ~{note[4:0],feedback_2};
-        else  if (~nrst)
-            note <= 6'b101100; 
-            else 
-            note <= {note[4:0],feedback_2};
+            next_note = ~{note[4:0],feedback_2};
+    end
+
+    always_ff @ (posedge clk, negedge nrst) begin 
+        if (~nrst)
+            note <= 6'b101101;
+        else 
+            note <= next_note;
     end
 endmodule 
 
