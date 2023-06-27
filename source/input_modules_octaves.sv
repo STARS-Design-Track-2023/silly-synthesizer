3cfbf90571888730b99e3620ded730da6cb86649
STARSFinalProject <chemist777@icloud.com>
2023-06-27 15:47:32 -0400

    Octaves FSM
    FSM for octave module for synth

--- source/input_modules_octaves.sv
+++ source/input_modules_octaves.sv
@@ -1,6 +1,6 @@
 module octaves
   (input logic clk, n_rst,octave_key_up,octave_key_down, 
-   output logic octave); 
+   output logic [2:0] octave); 
 
 
 typedef enum logic [2:0] {s1, s2, s3, s4, s5} 
@@ -11,16 +11,27 @@ state_t next_state;
 always_ff @(posedge clk, negedge n_rst) 
 begin 
   if(n_rst==0) begin 
-  state<=s1; 
-end 
+    state<=s3; 
+  end 
   else 
+  begin
+    state<=next_state; 
+  end 
+end
+
+always_comb
 begin
-  state<=next_state; 
-end 
-end 
+  case (state)
+    s1: octave = 3'd0;
+    s2: octave = 3'd1;
+    s3: octave = 3'd2;
+    s4: octave = 3'd3;
+    s5: octave = 3'd4;
+    default: octave = 3'd2;
+  endcase
+end
  
- 
-  always_comb 
+always_comb 
 begin 
 next_state=state;
  case(state) 
