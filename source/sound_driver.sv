8159bd7d2deacbd47ffc5e8b155ce0916c5c8e37
Noah Rediker <nrediker@purdue.edu>
2023-06-29 14:35:59 -0400

    Edge case bodges

--- source/sound_driver.sv
+++ source/sound_driver.sv
@@ -6,15 +6,31 @@ module sound_driver (
     output logic pwm_output
 );
 
-logic [17:0] signal;
-logic [7:0] scaled_signal, wave;
+logic [17:0] signal, signal_buffer, signal_buffer1, signal_buffer2, divider_buffer, divider_buffer1, divider_buffer2;
+logic [7:0] scaled_signal, scaled_buffer, wave;
 logic pulse, pwm_signal;
 
-oscillator sig_gen (divider, clk, nrst, signal);
+oscillator sig_gen (divider_buffer, clk, nrst, signal);
 clock_div39kHz sample_rate (clk, nrst, pulse);
-sequential_divider div (clk, nrst, pulse, divider, signal, scaled_signal);
-waveshaper shaper (divider, signal, scaled_signal, mode, wave);
+sequential_divider div (clk, nrst, pulse, divider_buffer1, signal_buffer1, scaled_signal);
+waveshaper shaper (divider_buffer, signal_buffer, scaled_buffer, mode, wave);
 pwm output_gen (clk, nrst, strobe, wave, pwm_signal);
+
+always_ff @(posedge clk, negedge nrst) begin
+    if (~nrst) begin
+        signal_buffer <= 0;
+        divider_buffer <= 0;
+        scaled_buffer <= 0;
+    end else if (pulse) begin
+        signal_buffer <= signal_buffer2;
+        signal_buffer1 <= signal;
+        signal_buffer2 <= signal_buffer1;
+        divider_buffer <= divider_buffer2;
+        divider_buffer1 <= divider;
+        divider_buffer2 <= divider_buffer1;
+        scaled_buffer <= scaled_signal;
+    end
+end
 
 always_ff @(posedge clk, negedge nrst) begin
     if (~nrst)
