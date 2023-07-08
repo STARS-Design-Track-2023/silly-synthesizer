73beb10c5ee9596ae9a8d6bc7086afee61de149d
Noah <nrediker@purdue.edu>
2023-07-07 23:20:33 -0400

    Update top_level_wrapper.sv

--- source/top_level_wrapper.sv
+++ source/top_level_wrapper.sv
@@ -10,17 +10,26 @@ module top_level_wrapper (
     output logic [33:0] gpio_oeb // Active Low Output Enable
 );
 
-    logic block_reset, block_reset_int, gated_reset;
+    // Reset Synchronizer
+    logic ff1, ff2;
     assign gated_reset = ~ncs & nrst;
 
-    // Reset Synchronizer
-    always_ff @ (posedge clk, negedge gated_reset) begin : RESET_SYNCHRONIZER
-        block_reset_int <= 1'b1;
-        block_reset <= block_reset_int;
+    always_ff @(posedge clk, negedge gated_reset) begin
+        if(~gated_reset) begin
+            ff1 <= 1'b0; 
+            ff2 <= 1'b0; 
+        end
+        else begin
+            ff1 <= 1'b1; 
+            ff2 <= ff1; 
+        end
+
     end
 
     assign gpio_oeb = {1'b0, {33{1'b1}}};
 
-    silly_synthesizer synth (clk, block_reset, ncs, gpio_in[16:0], gpio_out[33]);
+    silly_synthesizer synth (clk, ff2, ncs, gpio_in[16:0], gpio_out[33]);
+
+    assign gpio_out[32:0] = {33{1'b0}};
 
 endmodule
